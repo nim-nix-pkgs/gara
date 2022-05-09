a6a9b74657bd754e51034c80ba90a8c07ce1776e
hugosenari <hugosenari@gmail.com>
2022-05-09 20:37:32 -0300

    chore: re index gara

--- flake.nix
+++ flake.nix
@@ -7,14 +7,6 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
-  inputs."gara-master".dir   = "master";
-  inputs."gara-master".owner = "nim-nix-pkgs";
-  inputs."gara-master".ref   = "master";
-  inputs."gara-master".repo  = "gara";
-  inputs."gara-master".type  = "github";
-  inputs."gara-master".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."gara-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
